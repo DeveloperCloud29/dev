659b76858cfb648a447c5d3a1d8434c08740274e
DeveloperCloud29 <141287487+DeveloperCloud29@users.noreply.github.com>
2024-02-20 17:24:31 +0400

    Update settings.py

--- elevate/settings.py
+++ elevate/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7z46s&)a&n^^#vg%gc^v_038ok^jl!75^oq*1b3c#mxy#blfi-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
